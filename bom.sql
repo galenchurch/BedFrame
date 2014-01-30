57479acebc889dee0b99e4a7025eb2cebf5dc21b
Galen Church <galenechurch@gmail.com>
2014-01-30 16:08:21 -0500

    Initial

---
+++ bom.sql
@@ -0,0 +1,71 @@
+-- MySQL dump 10.13  Distrib 5.1.69, for redhat-linux-gnu (x86_64)
+--
+-- Host: 192.237.246.191    Database: bndr_s000
+-- ------------------------------------------------------
+-- Server version	5.1.69
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bom`
+--
+
+DROP TABLE IF EXISTS `bom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bom` (
+  `item` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `part` varchar(16) DEFAULT NULL,
+  `parent` int(10) unsigned DEFAULT NULL,
+  `parent_pn` varchar(16) DEFAULT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  `qty` int(10) unsigned DEFAULT NULL,
+  `unit` varchar(4) DEFAULT NULL,
+  `type` varchar(5) DEFAULT NULL,
+  `manufacturer` varchar(64) DEFAULT NULL,
+  `manufacturer_pn` varchar(64) DEFAULT NULL,
+  `supplier` varchar(64) DEFAULT NULL,
+  `supplier_pn` varchar(64) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL,
+  `files` varchar(255) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `cost_u` float DEFAULT NULL,
+  `cost_e` float DEFAULT NULL,
+  `types` varchar(255) DEFAULT NULL,
+  `tuples` varchar(255) DEFAULT NULL,
+  `top` tinyint(1) DEFAULT NULL,
+  `chksum` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`item`),
+  KEY `item` (`item`),
+  KEY `part` (`part`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bom`
+--
+
+LOCK TABLES `bom` WRITE;
+/*!40000 ALTER TABLE `bom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bom` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-01-30 16:08:21
